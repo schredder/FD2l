40975610854d4aa99a8b54317a5bb5806636f434
Yulian Kuncheff <yulian@kuncheff.com>
2012-02-17 03:34:55 -0800

    Attempt to fix the test.

--- test/test-tests.js
+++ test/test-tests.js
@@ -1,27 +1,23 @@
-$(document).ready(function(){
-    
-	test("a basic test example", function() {
-		ok( true, "this test is fine" );
-		var value = "hello";
-		equal( value, "hello", "We expect value to be hello" );
-	});
+test("a basic test example", function() {
+	ok( true, "this test is fine" );
+	var value = "hello";
+	equal( value, "hello", "We expect value to be hello" );
+});
 
-	module("Module A");
+module("Module A");
 
-	test("first test within module", function() {
-		ok( true, "all pass" );
-	});
+test("first test within module", function() {
+	ok( true, "all pass" );
+});
 
-	test("second test within module", function() {
-		ok( true, "all pass" );
-	});
+test("second test within module", function() {
+	ok( true, "all pass" );
+});
 
-	module("Module B");
+module("Module B");
 
-	test("some other test", function() {
-		expect(2);
-		equal( true, false, "failing test" );
-		equal( true, true, "passing test" );
-	});
-
+test("some other test", function() {
+	expect(2);
+	equal( true, false, "failing test" );
+	equal( true, true, "passing test" );
 });
