52ea563ec1f8e2ae14ebd472a54515781bbd920e
Yulian Kuncheff <yulian@kuncheff.com>
2012-02-17 03:42:29 -0800

    Removed faulty test.

--- test/test-tests.js
+++ test/test-tests.js
@@ -13,11 +13,3 @@ test("first test within module", function() {
 test("second test within module", function() {
 	ok( true, "all pass" );
 });
-
-module("Module B");
-
-test("some other test", function() {
-	expect(2);
-	equal( true, false, "failing test" );
-	equal( true, true, "passing test" );
-});
