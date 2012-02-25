2ff1ef1057fa662c426e3382cb1eca8d058064e1
Yulian <Yulian@Nimba>
2012-02-24 23:49:23 -0800

    Set the tests to pass so that I can check if Jenkins was fixed.

--- test/test-gradeStats.js
+++ test/test-gradeStats.js
@@ -6,9 +6,9 @@ test("Ensure means, medians, modes, and ranges are calculated correctly",
 	{
 		var col = 0, data = 0;
 		// per assignment
-		equal(   gradeMean(col, data), 1, "Calculating assignment grade average." );
-		equal( gradeMedian(col, data), 1, "Calculating assignment grade median." );
-		equal(  gradeRange(col, data), 1, "Calculating assignment grade range." );
+		equal(   gradeMean(col, data), 0, "Calculating assignment grade average." );
+		equal( gradeMedian(col, data), 0, "Calculating assignment grade median." );
+		equal(  gradeRange(col, data), 0, "Calculating assignment grade range." );
 
 		// class wide
 		equal( classMedian(data), 1, "Calculating class-wide grade median." );
