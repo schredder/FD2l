279ce6833ba933814758c78e8d1b9c8b463db517
Eric Schroeder <esschroeder@me.com>
2012-02-24 20:25:18 -0800

    More shuffling of gradeStats tests and methods.

--- src/gradeStats.js
+++ src/gradeStats.js
@@ -16,20 +16,27 @@ function gradeMedian(column, data) {
 	return 0;
 }
 
-// Calculates grade mode for a particular column
-// params: column - name of column to calculate from
-//               data - student data
-// return:     number - grade mode for a particular column
-function gradeMode(column, data) {
-	// TODO
-	return 0;
-}
-
 // Calculates grade range for a particular column
 // params: column - name of column to calculate from
 //               data - student data
 // return:     number - grade range for a particular column
 function gradeRange(column, data) {
+	// TODO
+	return 0;
+}
+
+// Calculates grade median for the whole class
+// params:   data - student data
+// return: number - median grade for a particular column
+function classMedian(data) {
+	// TODO
+	return 0;
+}
+
+// Calculates grade range for the whole class
+// params:   data - student data
+// return: number - median grade for a particular column
+function classRange(data) {
 	// TODO
 	return 0;
 }
--- test/test-gradeStats.js
+++ test/test-gradeStats.js
@@ -6,15 +6,11 @@ test("Ensure means, medians, modes, and ranges are calculated correctly",
 	{
 		var col = 0, data = 0;
 		// per assignment
-		equal(   gradeMean(col, data), 0, "Calculating assignment grade average." );
-		equal( gradeMedian(col, data), 0, "Calculating assignment grade median." );
-		equal(   gradeMode(col, data), 0, "Calculating assignment grade mode." );
-		equal(  gradeRange(col, data), 0, "Calculating assignment grade range." );
+		equal(   gradeMean(col, data), 1, "Calculating assignment grade average." );
+		equal( gradeMedian(col, data), 1, "Calculating assignment grade median." );
+		equal(  gradeRange(col, data), 1, "Calculating assignment grade range." );
 
 		// class wide
-		equal(   gradeMean(col, data), 0, "Calculating class-wide grade average." );
-		equal( gradeMedian(col, data), 0, "Calculating class-wide grade median." );
-		equal(   gradeMode(col, data), 0, "Calculating class-wide grade mode." );
-		equal(  gradeRange(col, data), 0, "Calculating class-wide grade range." );
+		equal( classMedian(data), 1, "Calculating class-wide grade median." );
 	}
 );
