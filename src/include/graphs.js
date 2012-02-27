e506d2e594b1bd961220de52fdf0a832b4c01428
Yulian <Yulian@Nimba.sjsu.edu>
2012-02-27 15:31:30 -0800

    Added some backing CSS and Graphs. Renamed "index.htm" -> "index.html" | I find .htm incorrect, even if its valid.

--- src/include/graphs.js
+++ src/include/graphs.js
@@ -1,8 +1,13 @@
 window.onload = function () {
-    var r = Raphael("holder"),
-        pie = r.piechart(320, 240, 100, [10, 20, 13, 32, 5], { legend: ["%%.%% - A", "%%.%% B", "%%.%% C", "%%.%% D", "%%.%% F"], legendpos: "west"});
+    drawClassGradeBreakdown();
+    drawStudentsClassStanding();
+};
 
-    r.text(320, 100, "Interactive Pie Chart").attr({ font: "20px sans-serif" });
+function drawClassGradeBreakdown() {
+    var r = Raphael("classBreakdown"),
+        pie = r.piechart(200, 200, 100, [10, 20, 13, 32, 5], { legend: ["%%.%% A", "%%.%% B", "%%.%% C", "%%.%% D", "%%.%% F"], legendpos: "east"});
+
+    r.text(260, 50, "Class Grade Breakdown").attr({ font: "20px sans-serif" });
     pie.hover(function () {
         this.sector.stop();
         this.sector.scale(1.1, 1.1, this.cx, this.cy);
@@ -20,4 +25,36 @@ window.onload = function () {
             this.label[1].attr({ "font-weight": 400 });
         }
     });
-};
+}
+
+function drawStudentsClassStanding() {
+    var r = Raphael("classStanding"),
+        fin = function () {
+            this.flag = r.popup(this.bar.x, this.bar.y, this.bar.value || "0").insertBefore(this);
+        },
+        fout = function () {
+            this.flag.animate({opacity: 0}, 300, function () {this.remove();});
+        },
+        fin2 = function () {
+            var y = [], res = [];
+            for (var i = this.bars.length; i--;) {
+                y.push(this.bars[i].y);
+                res.push(this.bars[i].value || "0");
+            }
+            this.flag = r.popup(this.bars[0].x, Math.min.apply(Math, y), res.join(", ")).insertBefore(this);
+        },
+        fout2 = function () {
+            this.flag.animate({opacity: 0}, 300, function () {this.remove();});
+        },
+        txtattr = { font: "12px sans-serif" };
+
+    r.text(160, 10, "Single Series Chart").attr(txtattr);
+    r.text(480, 10, "Multiline Series Stacked Chart").attr(txtattr);
+    r.text(160, 250, "Multiple Series Chart").attr(txtattr);
+    r.text(480, 250, "Multiline Series Stacked Chart\nColumn Hover").attr(txtattr);
+
+    r.barchart(10, 10, 300, 220, [[55, 20, 13, 32, 5, 1, 2, 10]]).hover(fin, fout);
+    r.hbarchart(330, 10, 300, 220, [[55, 20, 13, 32, 5, 1, 2, 10], [10, 2, 1, 5, 32, 13, 20, 55]], {stacked: true}).hover(fin, fout);
+    r.hbarchart(10, 250, 300, 220, [[55, 20, 13, 32, 5, 1, 2, 10], [10, 2, 1, 5, 32, 13, 20, 55]]).hover(fin, fout);
+    var c = r.barchart(330, 250, 300, 220, [[55, 20, 13, 32, 5, 1, 2, 10], [10, 2, 1, 5, 32, 13, 20, 55]], {stacked: true, type: "soft"}).hoverColumn(fin2, fout2);
+}
