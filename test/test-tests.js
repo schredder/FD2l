4a8e8ffc29337f06583ec58d99c96d1a8d4e9195
Bob Chatman III <bob@gneu.org>
2012-03-21 22:14:15 -0700

    Adding test for tomorrow. If you guys get a moment please review.

--- test/test-tests.js
+++ test/test-tests.js
@@ -54,3 +54,20 @@ test("Ensure that the object returned has repo identifiers as keys, and the appr
 		requestCSV("gradesExampleFixed.csv", fn);
 	}
 );
+
+
+module("Visualization");
+
+test("Ensure that the assignment list creates a dropdown of assignments",
+	function()
+	{
+		var testObj = {section:"",magic:"",repo:"$CATAGORY",Total:"totalgrade",Grade:"",q1:"quiz1",q2:"quiz2",
+					   q3:"quiz3",q4:"quiz",q5:"quiz",m1:"midterm",m2:"midterm",m3:"midterm",m4:"midterm",final:"final",
+					   hw1:"hw",hw2:"hw",hw3:"hw",hw4:"hw",hw5:"hw",hw6:"hw",hw7:"hw",HWFeedback:""};
+		var expectedObj = {quiz:{},midterm:{},final:{},hw:{}};
+		
+		expect(1);
+		equal(getAssignmentList(getCatagories({})), "<SELECT NAME='assignment_list'><OPTION VALUE=''>No Assignments Available</OPTION></SELECT>", "Empty Dropdown Generated Properly");
+		equal(getCatagories(testObj), "<SELECT NAME='assignment_list'><OPTION VALUE='quiz1'>quiz1</OPTION><OPTION VALUE='quiz2'>quiz2</OPTION><OPTION VALUE='quiz3'>quiz3</OPTION><OPTION VALUE='quiz'>quiz</OPTION>		<OPTION VALUE='midterm'>midterm</OPTION><OPTION VALUE='final'>final</OPTION><OPTION VALUE='hw'>hw</OPTION></SELECT>", "Filled Dropdown Generated Properly" );
+	}
+);
